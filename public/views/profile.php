476464cebd46f5e96933a97737ac3fa7a0ed4dc4
MRiabcew <rudy2305@icloud.com>
2023-01-16 18:23:32 +0100

    PHP routing

---
+++ public/views/profile.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<HTML lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device=width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="public/css/profile.css">
+        <title>Profile</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="card">
+                <img src="img.jpg" alt="John" style="width:100%">
+                <h1>John Doe</h1>
+                <p class="title">CEO & Founder, Example</p>
+                <p>Harvard University</p>
+                <a href="#"><i class="fa fa-dribbble"></i></a>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="#"><i class="fa fa-linkedin"></i></a>
+                <a href="#"><i class="fa fa-facebook"></i></a>
+                <p><button>Contact</button></p>
+            </div>
+        </div>
+
+        <div class="movies">
+            
+        </div>
+</body>
+
+    
+</HTML>
---
+++ public/views/register.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<head>
+    <link rel="stylesheet" type="text/css" href="pablic/css/style.css">
+    <title>MyMovie-Register</title>
+</head>
+<body>
+    <div class="container">
+        <div class="logo">
+            <img src="public/img/logo.svg">
+        </div>
+        <form>
+            <input name="username" type="text" placeholder="username">
+            <input name="email" type="text" placeholder="email@email.com">
+            <input name="password" type="password" placeholder="password">
+            <input name="password" type="password" placeholder="password">
+            <input name="regulations" type="checkbox" id="regulations" value="a">
+            <label for="regulations">Accept regulations</label>
+            <button>REGISTER</button>
+        </form>
+    </div>
+</body>
